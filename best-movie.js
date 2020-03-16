c5c67db13ffa039c1695b3e487122066658dc8e0
vincentiga <61868081+vincentiga@users.noreply.github.com>
2020-03-16 23:03:41 +0100

    Create best-movie.js

---
+++ best-movie.js
@@ -0,0 +1,8 @@
+/*best-movie*/
+//Define some variables
+const nameMovie = 'The Green Miles';
+const nameDirector = 'Frank Darabont';
+const message = '' + nameMovie + ', realized by ' + nameDirector + ', came out in 1999';
+
+//Display dispay a popup
+alert(message);
