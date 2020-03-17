76bf043d5ad80d2f6ffa4f9bb57e5df48e5d023a
Vincent <igabillevincent@outlook.fr>
2020-03-17 15:01:02 +0100

    modification of two files

--- best-movie.js
+++ best-movie.js
@@ -2,7 +2,8 @@
 //Define some variables
 const nameMovie = 'The Green Miles';
 const nameDirector = 'Frank Darabont';
-const message = '' + nameMovie + ', realized by ' + nameDirector + ', came out in 1999';
+const whenCameOut = '1999';
+const message = '' + nameMovie + ', realized by ' + nameDirector + ', in ' + whenCameOut + '';
 
 //Display dispay a popup
 alert(message);
